ecb9c9f42bcd8f1aa96188913ab1d2e89e0c0fc6
shubham4717211 <118740802+shubham4717211@users.noreply.github.com>
2023-02-03 16:39:02 +0530

    adding route for isNew api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,84 @@
-import { Router } from "express"
+import { Router, Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { Product } from '../modules/product/entities/product.entity';
 
-export default (rootDirectory, pluginOptions) => {
-  const router = Router()
 
-  router.get("/hello", (req, res) => {
-    res.json({
-      message: "Welcome to My Store!",
-    })
-  })
+const router = Router();
 
-  return router
-}
+// Create a new product
+router.post('/products', async (req: Request, res: Response) => {
+  const product = new Product();
+  product.name = req.body.name;
+  product.description = req.body.description;
+  product.price = req.body.price;
+
+  try {
+    const savedProduct = await getRepository(Product).save(product);
+    return res.status(201).json(savedProduct);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// Get all products
+router.get('/products', async (req: Request, res: Response) => {
+  try {
+    const products = await getRepository(Product).find();
+    return res.json(products);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// Get a single product by ID
+router.get('/products/:id', async (req: Request, res: Response) => {
+  try {
+    const product = await getRepository(Product).findOne(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    return res.json(product);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// Update a product
+router.put('/products/:id', async (req: Request, res: Response) => {
+  try {
+    const product = await getRepository(Product).findOne(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    product.name = req.body.name;
+    product.description = req.body.description;
+    product.price = req.body.price;
+
+    const savedProduct = await getRepository(Product).save(product);
+    return res.json(savedProduct);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// Delete a product
+router.delete('/products/:id', async (req: Request, res: Response) => {
+  try {
+    const product = await getRepository(Product).findOne(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    await getRepository(Product).delete(req.params.id);
+    return res.json({ message: 'Product deleted successfully' });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+export default router;
